da10ea45e0856cebef871a8ab25b5021d49e7e1c
LeftShiftJones <ryan_jones5@taylor.edu>
2018-12-13 00:20:52 -0500

    Added SQL inserts

---
+++ ryan_jones_public_members.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.members (email, password, name) VALUES ('ariel@gmail.com', 'Password1', 'Ariel Wentworth');
+INSERT INTO public.members (email, password, name) VALUES ('alice@gmail.com', 'Password1', 'Alice');
+INSERT INTO public.members (email, password, name) VALUES ('ryan@gmail.com', 'Password1', 'Ryan Jones');
+INSERT INTO public.members (email, password, name) VALUES ('bob@gmail.com', 'Password1', 'Robert');
